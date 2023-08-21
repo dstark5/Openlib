d253e5297160c103402551e07fac159f52b67216
Piyush Suthar <piyushsuthar100@gmail.com>
2023-08-21 20:30:10 +0530

    fixed search page

--- lib/ui/search_page.dart
+++ lib/ui/search_page.dart
@@ -39,19 +39,23 @@ class SearchPage extends ConsumerWidget {
               padding: const EdgeInsets.only(left: 7, right: 7, top: 10),
               child: TextField(
                 showCursor: true,
-                cursorColor: Theme.of(context).colorScheme.secondary,
+                // cursorColor: Theme.of(context).colorScheme.secondary,
+
                 decoration: InputDecoration(
-                  enabledBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black45, width: 2),
-                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  label: const Text("Search"),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.outline, width: 2),
+                    borderRadius: const BorderRadius.all(Radius.circular(50)),
                   ),
-                  focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black54, width: 2),
-                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.outline, width: 2),
+                    borderRadius: const BorderRadius.all(Radius.circular(50)),
                   ),
                   suffixIcon: IconButton(
-                    padding: const EdgeInsets.only(right: 5),
-                    color: Theme.of(context).colorScheme.secondary,
+                    padding: const EdgeInsets.only(right: 10),
+                    // color: Theme.of(context).colorScheme.secondary,
                     icon: const Icon(
                       Icons.search,
                       size: 23,
@@ -59,100 +63,139 @@ class SearchPage extends ConsumerWidget {
                     onPressed: () => onSubmit(context, ref),
                   ),
                   filled: true,
-                  hintStyle: const TextStyle(
-                      color: Colors.grey, fontWeight: FontWeight.bold),
-                  hintText: "Search",
-                  fillColor: Theme.of(context).colorScheme.primary,
+                  // hintStyle: const TextStyle(
+                  //     color: Colors.grey, fontWeight: FontWeight.bold),
+                  hintText: "Harry Potter and th...",
+                  // fillColor: Theme.of(context).colorScheme.primary,
                 ),
                 onSubmitted: (String value) => onSubmit(context, ref),
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12,
+                style: const TextStyle(
+                  // color: Theme.of(context).colorScheme.tertiary,
+                  // fontWeight: FontWeight.bold,
+                  fontSize: 14,
                 ),
                 onChanged: (String value) {
                   ref.read(searchQueryProvider.notifier).state = value;
                 },
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 7, right: 7, top: 19),
-              child: SizedBox(
-                width: 250,
-                child: DropdownButtonFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Type',
-                    labelStyle: TextStyle(
-                      fontSize: 11,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).colorScheme.secondary,
-                    ),
-                    enabledBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black54, width: 2),
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black54, width: 2),
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 7, right: 7, top: 19),
+                  child: SizedBox(
+                    width: 200,
+                    child: DropdownButtonFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Type',
+                        labelStyle: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.outline,
+                              width: 2),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(50)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.outline,
+                              width: 2),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(50)),
+                        ),
+                      ),
+                      icon: const Icon(Icons.arrow_drop_down),
+                      value: dropdownTypeValue,
+                      items: typeValues.keys
+                          .toList()
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: const TextStyle(fontSize: 14),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? val) {
+                        ref.read(selectedTypeState.notifier).state = val ?? '';
+                      },
                     ),
                   ),
-                  icon: const Icon(Icons.arrow_drop_down),
-                  value: dropdownTypeValue,
-                  items: typeValues.keys
-                      .toList()
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(
-                        value,
-                        style: const TextStyle(fontSize: 12),
-                      ),
-                    );
-                  }).toList(),
-                  onChanged: (String? val) {
-                    ref.read(selectedTypeState.notifier).state = val ?? '';
-                  },
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 7, right: 7, top: 19),
+                  child: SizedBox(
+                    width: 150,
+                    child: DropdownButtonFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Sort by',
+                        labelStyle: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.outline,
+                              width: 2),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(50)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.outline,
+                              width: 2),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(50)),
+                        ),
+                      ),
+                      value: dropdownSortValue,
+                      items: sortValues.keys
+                          .toList()
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: const TextStyle(fontSize: 14),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? val) {
+                        ref.read(selectedSortState.notifier).state = val ?? '';
+                      },
+                    ),
+                  ),
+                )
+              ],
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 7, right: 7, top: 19),
-              child: SizedBox(
-                width: 210,
-                child: DropdownButtonFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Sort by',
-                    labelStyle: TextStyle(
-                      fontSize: 11,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).colorScheme.secondary,
-                    ),
-                    enabledBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black45, width: 2),
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black54, width: 2),
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                    ),
-                  ),
-                  value: dropdownSortValue,
-                  items: sortValues.keys
-                      .toList()
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(
-                        value,
-                        style: const TextStyle(fontSize: 12),
-                      ),
-                    );
-                  }).toList(),
-                  onChanged: (String? val) {
-                    ref.read(selectedSortState.notifier).state = val ?? '';
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(),
+                  onPressed: () {
+                    onSubmit(context, ref);
                   },
-                ),
-              ),
+                  child: const Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Search",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8),
+                        child: Icon(Icons.search, size: 18),
+                      ),
+                    ],
+                  )),
             )
           ],
         ),
