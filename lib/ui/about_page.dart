0db7794455c3cc5ab69c5d1895e5923261ce9de9
dstark5 <dineshvkl05@protonmail.com>
2023-08-25 05:02:47 -0700

    added about page

---
+++ lib/ui/about_page.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'package:openlib/ui/components/snack_bar_widget.dart';
+import 'package:openlib/ui/components/page_title_widget.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: const Text("Openlib"),
+        titleTextStyle: Theme.of(context).textTheme.displayLarge,
+      ),
+      body: const SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: EdgeInsets.only(left: 5, right: 5, top: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TitleText("About"),
+              Padding(
+                padding:
+                    EdgeInsets.only(left: 7, right: 7, top: 13, bottom: 10),
+                child: Text(
+                  "An Open source app to download and read books from shadow library (Anna`s Archive)",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 7, right: 7, top: 10),
+                child: Text(
+                  "Github",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              _UrlText(
+                text: 'Open Github Page',
+                url: 'https://github.com/dstark5/Openlib',
+              ),
+              _UrlText(
+                  text: 'Contribute To Openlib',
+                  url:
+                      'https://github.com/dstark5/Openlib/blob/main/CONTRIBUTING.md'),
+              _UrlText(
+                  text: 'Report An Issue',
+                  url: 'https://github.com/dstark5/Openlib/issues'),
+              Padding(
+                padding: EdgeInsets.only(left: 7, right: 7, top: 10),
+                child: Text(
+                  "Licence",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              _UrlText(
+                  text: "GPL v3.0 license",
+                  url: 'https://www.gnu.org/licenses/gpl-3.0.en.html'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _UrlText extends StatelessWidget {
+  const _UrlText({Key? key, required this.text, required this.url})
+      : super(key: key);
+
+  final String url;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 7, right: 7, top: 5),
+      child: InkWell(
+        onTap: () async {
+          final Uri uri = Uri.parse(url);
+          if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+            // ignore: use_build_context_synchronously
+            showSnackBar(context: context, message: 'Could not launch $uri');
+          }
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: const TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
+              ),
+            ),
+            const SizedBox(
+              width: 2,
+            ),
+            const Icon(
+              Icons.launch,
+              size: 20,
+              color: Colors.blueAccent,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/settings_page.dart
+++ lib/ui/settings_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:openlib/ui/components/page_title_widget.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:openlib/ui/components/page_title_widget.dart';
+import 'package:openlib/ui/about_page.dart';
 import 'package:openlib/state/state.dart' show themeModeProvider, dbProvider;
 
 class SettingsPage extends ConsumerWidget {
@@ -17,45 +19,80 @@ class SettingsPage extends ConsumerWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const TitleText("Settings"),
-            Padding(
-              padding: const EdgeInsets.only(left: 5, right: 5, top: 10),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: Theme.of(context).colorScheme.tertiaryContainer,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Dark Mode",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).colorScheme.tertiary,
-                        ),
-                      ),
-                      Switch(
-                        // This bool value toggles the switch.
-                        value: ref.watch(themeModeProvider) == ThemeMode.dark,
-                        activeColor: Colors.red,
-                        onChanged: (bool value) {
-                          ref.read(themeModeProvider.notifier).state =
-                              value == true ? ThemeMode.dark : ThemeMode.light;
-                          ref
-                              .read(dbProvider)
-                              .savePreference('darkMode', value);
-                        },
-                      )
-                    ],
+            _PaddedContainer(
+              children: [
+                Text(
+                  "Dark Mode",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.tertiary,
                   ),
                 ),
-              ),
+                Switch(
+                  // This bool value toggles the switch.
+                  value: ref.watch(themeModeProvider) == ThemeMode.dark,
+                  activeColor: Colors.red,
+                  onChanged: (bool value) {
+                    ref.read(themeModeProvider.notifier).state =
+                        value == true ? ThemeMode.dark : ThemeMode.light;
+                    ref.read(dbProvider).savePreference('darkMode', value);
+                  },
+                )
+              ],
             ),
+            _PaddedContainer(
+              onClick: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                  return const AboutPage();
+                }));
+              },
+              children: [
+                Text(
+                  "About",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.tertiary,
+                  ),
+                ),
+              ],
+            )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PaddedContainer extends StatelessWidget {
+  const _PaddedContainer({Key? key, this.onClick, required this.children})
+      : super(key: key);
+
+  final VoidCallback? onClick;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 5, right: 5, top: 10),
+      child: InkWell(
+        onTap: onClick,
+        child: Container(
+          height: 61,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Theme.of(context).colorScheme.tertiaryContainer,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: children,
+            ),
+          ),
         ),
       ),
     );
