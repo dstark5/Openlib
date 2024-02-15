cddaeb553a3dda1a8ee1d7573a04c7f3d86bfa10
dstark5 <dineshvkl05@protonmail.com>
2024-02-15 22:31:16 +0530

    updated version number

--- lib/ui/about_page.dart
+++ lib/ui/about_page.dart
@@ -43,7 +43,7 @@ class AboutPage extends StatelessWidget {
               Padding(
                 padding: EdgeInsets.only(left: 7, right: 7, top: 5),
                 child: Text(
-                  "1.0.5",
+                  "1.0.6",
                   style: TextStyle(
                       fontSize: 15,
                       fontWeight: FontWeight.bold,
