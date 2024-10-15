fc5f132d6ec76c6a1b86616f40356a6846d706b6
Naveen jangra <nav0813jangra@gmail.com>
2024-10-15 21:54:11 +0530

    added category info and thumbnails

--- lib/ui/categories_page.dart
+++ lib/ui/categories_page.dart
@@ -19,41 +19,71 @@ class CategoryBook {
   final String title;  
   final String thumbnail;
   final String tag;
-
-  CategoryBook({required this.title, required this.thumbnail,required this.tag});
+  final String info;
+  CategoryBook({required this.title, required this.thumbnail,required this.tag, required this.info});
 }
 
 List<CategoryBook> categoriesTypeValues = [
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg",  title: "Classics", tag: "list/tag/classics"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Romance", tag: "list/tag/romance"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Fiction", tag: "list/tag/fiction"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Young Adult", tag: "list/tag/young-adult"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Fantasy", tag: "list/tag/fantasy"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Science Fiction", tag: "list/tag/science-fiction"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Nonfiction", tag: "list/tag/non-fiction"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Children", tag: "list/tag/children"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "History", tag: "list/tag/history"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Mystery", tag: "list/tag/mystery"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Covers", tag: "list/tag/covers"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Horror", tag: "list/tag/horror"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Historical Fiction", tag: "list/tag/historical-fiction"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Gay", tag: "list/tag/gay"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Best", tag: "list/tag/best"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Titles", tag: "list/tag/titles"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Middle Grade", tag: "list/tag/middle-grade"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Paranormal", tag: "list/tag/paranormal"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Love", tag: "list/tag/love"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Queer", tag: "list/tag/queer"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Nonfiction", tag: "list/tag/nonfiction"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Historical Romance", tag: "list/tag/historical-romance"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "LGBT", tag: "list/tag/lgbt"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Contemporary", tag: "list/tag/contemporary"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Thriller", tag: "list/tag/thriller"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Women", tag: "list/tag/women"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Biography", tag: "list/tag/biography"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "LGBTQ", tag: "list/tag/lgbtq"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Series", tag: "list/tag/series"),
-  CategoryBook(thumbnail: "https://static.vecteezy.com/system/resources/previews/022/692/148/non_2x/beautiful-image-of-lord-krishna-on-black-background-generative-ai-photo.jpeg", title: "Title Challenge", tag: "list/tag/title-challenge"),
+  CategoryBook( info : "Timeless literary works often revered for their artistic merit and cultural significance.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/1%20classic.jpeg", title: "Classics", tag: "list/tag/classics"),
+  CategoryBook( info : "Stories focused on romantic relationships, exploring love, passion, and emotional connections.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/2%20romance.jpeg", title: "Romance", tag: "list/tag/romance"),
+  CategoryBook( info : "Narrative literature created from the imagination, not based on real events.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/3%20fiction.jpeg", title: "Fiction", tag: "list/tag/fiction"),
+  CategoryBook( info : "Books targeted at teenage readers, addressing themes relevant to adolescence.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/4%20young%20adult.jpeg", title: "Young Adult", tag: "list/tag/young-adult"),
+  CategoryBook( info : "A genre featuring magical elements, mythical creatures, and fantastical worlds.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/5%20fantasy%20book.jpeg", title: "Fantasy", tag: "list/tag/fantasy"),
+  CategoryBook( info : "Literature that explores futuristic concepts, advanced technology, and space exploration.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/6%20science%20fiction.jpeg", title: "Science Fiction", tag: "list/tag/science-fiction"),
+  CategoryBook( info : "Works based on factual information, including essays, biographies, and documentaries.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/7%20non%20fiction.jpeg", title: "Nonfiction", tag: "list/tag/non-fiction"),
+  CategoryBook( info : "Books aimed at young readers, often with illustrations and simple narratives.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/8%20children.jpeg", title: "Children", tag: "list/tag/children"),
+  CategoryBook( info : "Literature that examines past events, cultures, and significant historical figures.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/9%20history.jpeg", title: "History", tag: "list/tag/history"),
+  CategoryBook( info : "Stories centered around suspenseful plots, often involving crime or puzzles to solve.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/10%20mystery.jpeg", title: "Mystery", tag: "list/tag/mystery"),
+  CategoryBook( info : "Refers to the artwork and design that visually represents a book, influencing reader interest.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/11%20covers.jpeg", title: "Covers", tag: "list/tag/covers"),
+  CategoryBook( info : "A genre designed to evoke fear, dread, or terror in the reader through suspenseful storytelling.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/12%20horror.jpeg", title: "Horror", tag: "list/tag/horror"),
+  CategoryBook( info : "Novels set in a specific historical period, blending factual events with fictional characters.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/13%20historical%20fiction.jpeg", title: "Historical Fiction", tag: "list/tag/historical-fiction"),
+  CategoryBook( info : "Literature that features male same-sex relationships and explores LGBTQ+ themes.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/14%20gay.jpeg", title: "Gay", tag: "list/tag/gay"),
+  CategoryBook( info : "Often refers to critically acclaimed or popular books, usually categorized by rankings or awards.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/15%20best%20.jpeg", title: "Best", tag: "list/tag/best"),
+  CategoryBook( info : "Refers to the names of books, which often reflect their themes or subject matter.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/16%20titles.jpeg", title: "Titles", tag: "list/tag/titles"),
+  CategoryBook( info : "Books intended for readers aged 8-12, featuring age-appropriate themes and characters.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/17%20middle%20grade.jpeg", title: "Middle Grade", tag: "list/tag/middle-grade"),
+  CategoryBook( info : "Stories that incorporate supernatural elements, including ghosts, vampires, and otherworldly beings.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/18%20paranormal.jpeg", title: "Paranormal", tag: "list/tag/paranormal"),
+  CategoryBook( info : "A theme exploring the complexities and nuances of love in various forms and relationships.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/19%20love.jpeg", title: "Love", tag: "list/tag/love"),
+  CategoryBook( info : "Literature that represents diverse sexual orientations and gender identities within the LGBTQ+ spectrum.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/20%20queer.jpeg", title: "Queer", tag: "list/tag/queer"),
+  CategoryBook( info : "Works based on factual information, including essays, biographies, and documentaries.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/21%20nonfictino.jpeg", title: "Nonfiction", tag: "list/tag/nonfiction"),
+  CategoryBook( info : "Novels combining romantic plots set against a historical backdrop.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/22%20historical%20romance.jpeg", title: "Historical Romance", tag: "list/tag/historical-romance"),
+  CategoryBook( info : "Literature encompassing themes related to lesbian, gay, bisexual, and transgender identities.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/23%20lgbt.jpeg", title: "LGBT", tag: "list/tag/lgbt"),
+  CategoryBook( info : "Works set in modern times, often addressing current social issues and relatable characters.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/24%20contemporary.jpeg", title: "Contemporary", tag: "list/tag/contemporary"),
+  CategoryBook( info : "A suspenseful genre focused on excitement, tension, and unexpected twists.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/25%20thriller.jpeg", title: "Thriller", tag: "list/tag/thriller"),
+  CategoryBook( info : "Literature that explores women's experiences, perspectives, and empowerment.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/26%20women.jpeg", title: "Women", tag: "list/tag/women"),
+  CategoryBook( info : "Nonfiction works detailing the life story of an individual, highlighting their achievements and challenges.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/27%20biography.jpeg", title: "Biography", tag: "list/tag/biography"),
+  CategoryBook( info : "Inclusive literature representing the experiences of the lesbian, gay, bisexual, transgender, and queer community.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/28%20lgbtq.jpeg", title: "LGBTQ", tag: "list/tag/lgbtq"),
+  CategoryBook( info : "A collection of related books that follow a common storyline or set of characters.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/29%20series%20.jpeg", title: "Series", tag: "list/tag/series"),
+  CategoryBook( info : "Refers to prompts or contests encouraging creative thinking about book titles.",
+  thumbnail: "https://raw.githubusercontent.com/Nav-jangra/images/refs/heads/main/30%20title%20chhallenge.jpeg", title: "Title Challenge", tag: "list/tag/title-challenge"),
 ];
 
 class CategoriesPage extends ConsumerWidget {
@@ -78,14 +108,14 @@ class CategoriesPage extends ConsumerWidget {
                     final category = categories[index];
                     return BookInfoCard(
                       title: category.title,
-                      thumbnail: category.thumbnail, // Handle null case
-                      info: category.tag, // define info 
+                      thumbnail: category.thumbnail, 
+                      info: category.info,  
                       onClick: () {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
                             builder: (BuildContext context) {
-                              return CategoryListingPage(url: category.tag, title: category.title,); // Update based on your logic
+                              return CategoryListingPage(url: category.tag, title: category.title,);
                             },
                           ),
                         );
@@ -180,10 +210,10 @@ class BookInfoCard extends StatelessWidget {
                       style: TextStyle(
                         fontSize: 25,
                         fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.tertiary,
+                        color: Theme.of(context).colorScheme.secondary,
                       ),
                       overflow: TextOverflow.ellipsis,
-                      maxLines: 3,
+                      maxLines: 2,
                     ),
                     Text(
                       info,
@@ -194,7 +224,7 @@ class BookInfoCard extends StatelessWidget {
                             Theme.of(context).textTheme.headlineMedium?.color,
                       ),
                       overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
+                      maxLines: 3,
                     ),
                   ],
                 ),
